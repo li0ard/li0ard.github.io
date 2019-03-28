3ee617f2fbf9bf25d6b05f33aef1927bec601dc0
NickProgramm <47205258+NickProgramm@users.noreply.github.com>
2019-03-28 11:51:27 +0600

    Update math.js

--- math.js
+++ math.js
@@ -1,19 +1,12 @@
-/*JS-фреймворк math.
-Перед вами новый JS-фреймворк math:
-Его особенности:
-	•Отделение функций фреймворка знаком $
-	•Вместо большого кода одна функция
-	•Можно дополнять свои функции
-	•Низкий риск получения ошибки
-	•Просто использовать
-https://vk.com/3peekawowd
-©2019 Nick*/
-//Умножение
+//Copy link to import code
+//https://vk.com/3peekawowd
+//©2019 Nick
+
 const $umn = function(x, y) {
 	const umn = x * y;
 	return umn;
 }
-//Сложение
+
 const $sl = function(x, y) {
 	const sl = x + y;
 	return sl;
@@ -23,32 +16,32 @@ const $vch = function(x, y) {
 	const vch = x - y;
 	return vch;
 }
-//Деление
+
 const $del = function(x, y) {
 	const del = x / y;
 	return del;
 }
-//Возвращение остатка
+
 const $mod = function(x, y) {
 	const mod = x % y;
 	return mod;
 }
-//Рандом
+
 const $rand = function(x, y) {
 	const rand = Math.randon(x, y);
 	return rand;
 }
-//Инкримент
+
 const $ink = function(x) {
 	const ink = x = x + 1;
 	return ink;
 }
-//Декримент
+
 const $dek = function(x) {
 	const dek = x = x - 1;
 	return dek;
 }
-//Факториал
+
 const $fct = function(n) {
     const fct = 1;
     while(n) {
@@ -56,7 +49,7 @@ const $fct = function(n) {
     }
     return fct;
 }
-//Квадратный Корень
+
 const $sqrt = function(x) {
     const sqrt = Math.sqrt(x);
     return sqrt;
