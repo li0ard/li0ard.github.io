75b4621ddadf74005b273bd65fa1af7420ac948b
NickProgramm <47205258+NickProgramm@users.noreply.github.com>
2019-03-31 11:26:29 +0600

    math.rb
    The math package is now available in ruby.

---
+++ math.rb
@@ -0,0 +1,48 @@
+def sl(a, b)
+
+    return a + b
+
+end
+
+def umn(c, d)
+
+    return c * d
+
+end
+
+def vch(e, f)
+
+    return e - f
+
+end
+
+
+def del(g, h)
+
+    return g / h
+
+end
+
+def mod(i, j)
+
+    return i % j
+
+end
+
+def fct(n)
+
+  (1..n).inject(:*) || 1
+
+end
+
+def randon(n)
+
+    return rand(n)
+
+end
+
+def mathsqrt(z)
+
+    return Math.sqrt(z)
+
+end
